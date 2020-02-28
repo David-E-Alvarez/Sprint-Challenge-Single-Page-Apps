4f80b806a05beff789f576ebbc6e1ba312503018
david <davidealvarez1@gmail.com>
2020-02-28 15:54:41 -0800

    update

--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -7,19 +7,19 @@ export default function CharacterList() {
   //TODO: Add useState to track data from useEffect
   const [characterData, setCharacterData] = useState([]);
 
-  useEffect(() => {
-    // TODO: Add API Request here - must run in `useEffect`
-    //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
-    axios
-      .get("https://cors-anywhere.herokuapp.com/https://rickandmortyapi.com/api/character/")
-      .then(response => {
-        console.log("response.data: ", response.data);
-        setCharacterData(response.data.results);
-      })
-      .catch(error => {
-        console.log("Data was not returned,", error);
-      });
-  }, []);
+  // useEffect(() => {
+  //   // TODO: Add API Request here - must run in `useEffect`
+  //   //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
+  //   axios
+  //     .get("https://cors-anywhere.herokuapp.com/https://rickandmortyapi.com/api/character/")
+  //     .then(response => {
+  //       console.log("response.data: ", response.data);
+  //       setCharacterData(response.data.results);
+  //     })
+  //     .catch(error => {
+  //       console.log("Data was not returned,", error);
+  //     });
+  // }, []);
  
   return (
     <section className="character-list">
--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -8,21 +8,21 @@ export default function SearchForm() {
     setQuery(event.target.value);
     //console.log("event: ", event.target.value);
   }
-  useEffect(() => {
-    // TODO: Add API Request here - must run in `useEffect`
-    //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
-    axios
-      .get("https://cors-anywhere.herokuapp.com/https://rickandmortyapi.com/api/character/")
-      .then(response => {
-        console.log("response.data.results: ", response.data.results);
-        const characters = response.data.results.filter(character => 
-          character.name.toLowerCase().includes(query.toLowerCase()))
-        setData(characters);
-      })
-      .catch(error => {
-        console.log("Data was not returned,", error);
-      });
-  }, [query]);
+  // useEffect(() => {
+  //   // TODO: Add API Request here - must run in `useEffect`
+  //   //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
+  //   axios
+  //     .get("https://cors-anywhere.herokuapp.com/https://rickandmortyapi.com/api/character/")
+  //     .then(response => {
+  //       console.log("response.data.results: ", response.data.results);
+  //       const characters = response.data.results.filter(character => 
+  //         character.name.toLowerCase().includes(query.toLowerCase()))
+  //       setData(characters);
+  //     })
+  //     .catch(error => {
+  //       console.log("Data was not returned,", error);
+  //     });
+  // }, [query]);
   const inputChange = event =>{
     setQuery(event.target.value);
   }
