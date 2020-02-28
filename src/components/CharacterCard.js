d13bde1c2ade2e4206a560ad15e206758519703c
david <davidealvarez1@gmail.com>
2020-02-28 08:46:27 -0800

    retrieving data and able to render in a reactstrap card

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,5 +1,14 @@
 import React from "react";
-
-export default function CharacterCard() {
-  return <span>todo: character</span>;
+import {
+  Card, CardBody, CardTitle} from 'reactstrap';
+export default function CharacterCard(props) {
+  return(
+    <div>
+      <Card>
+        <CardBody>
+          <CardTitle>{props.name}</CardTitle>
+        </CardBody>
+      </Card>
+    </div>
+  ); 
 }
--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,16 +1,30 @@
 import React, { useEffect, useState } from "react";
+import axios from "axios";
+import CharacterCard from "./CharacterCard.js";
 
 export default function CharacterList() {
   // TODO: Add useState to track data from useEffect
+  const [characterData, setCharacterData] = useState([]);
 
-  useEffect(() => {
-    // TODO: Add API Request here - must run in `useEffect`
-    //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
-  }, []);
+  // useEffect(() => {
+  //   // TODO: Add API Request here - must run in `useEffect`
+  //   //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
+  //   axios
+  //     .get("https://cors-anywhere.herokuapp.com/https://rickandmortyapi.com/api/character/")
+  //     .then(response => {
+  //       console.log("response.data: ", response.data);
+  //       setCharacterData(response.data.results);
+  //     })
+  //     .catch(error => {
+  //       console.log("Data was not returned,", error);
+  //     });
+  // }, []);
 
   return (
     <section className="character-list">
-      <h2>TODO: `array.map()` over your state here!</h2>
+      {characterData.map(character => {
+        return <CharacterCard name={character.name} />;
+      })}
     </section>
   );
 }
